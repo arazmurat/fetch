5c5eef82e5e71424b239b93e924d5bf718959e54
Arazmyrat <arazmirat@icloud.com>
2021-04-07 23:51:38 +0400

    use fetching data

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import UserList from "./UserList";
+
+function App() {
+  return (
+    <div className="App">
+      App Component
+      <UserList />
+    </div>
+  );
+}
+
+export default App;

5ceb8b46062a2a046d61576b6988cbc50a534cec
Arazmyrat <arazmirat@icloud.com>
2021-04-11 16:45:44 +0400

    added axios codes

--- src/UserList.js
+++ src/UserList.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 export default class UserList extends Component {
   state = { users: [], isLoading: true };
   //Created by "CDM" snippet
   componentDidMount() {
     setTimeout(() => {
-      fetch("https://jsonplaceholder.typicode.com/users")
-        .then((result) => result.json())
-        .then((jsonData) =>
-          this.setState({ users: jsonData, isLoading: false })
+      //fetch("https://jsonplaceholder.typicode.com/users")
+      //  .then((result) => result.json())
+      //  .then((jsonData) =>
+      //    this.setState({ users: jsonData, isLoading: false })
+      axios.get("https://jsonplaceholder.typicode.com/users")
+        .then((jsonData) =>this.setState({ users: jsonData.data, isLoading: false })
         );
     }, 3000);
   }
